c50a7d3cdb12b5d604655c45072533cde39a946b
Rus <ruzaki.dev@gmail.com>
2025-12-23 10:45:38 -0500

    Configure Release scheme and apply debug-specific background color in ProfileViewController

--- ZakirovHW_2_2/ZakirovHW_2_2/Profile/ProfileViewController.swift
+++ ZakirovHW_2_2/ZakirovHW_2_2/Profile/ProfileViewController.swift
@@ -17,7 +17,7 @@ class ProfileViewController: UIViewController {
     private let tableView: UITableView = {
         
         let tableView = UITableView()
-        tableView.backgroundColor = .white
+        // tableView.backgroundColor = .white
         tableView.translatesAutoresizingMaskIntoConstraints = false
         
         return tableView
@@ -78,7 +78,13 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
+        #if DEBUG
+        view.backgroundColor = .systemYellow
+        #else
+        view.backgroundColor = .systemIndigo
+        #endif
+        
+        // view.backgroundColor = .white
         title = "Profile"
         
         view.addSubview(tableView)
